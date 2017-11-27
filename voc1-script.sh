50e3e96c53a99097e15fc758177b1d69fda212ca
Михаил <mikhailnov@dumalogiya.ru>
2017-11-27 08:25:22 +0300

    it's finally working!

--- voc1-script.sh
+++ voc1-script.sh
@@ -14,23 +14,25 @@ rm -fv voc1*.list
 # the path to the text file from which we have to solve the crossword puzzle is stored in the environmental variable
 # it allows to specify it only once in the session and reuse it automatically for all following words
 
-if [[ -z "$VOC1_INPUT_TEXT_PATH" ]]
-	then
-		echo "input the path to the input text file and press enter"
-		read VOC1_INPUT_TEXT_PATH_ORIG
-		# when copying a file in the dolphin file manager and then pasting to the text field, the output has a prefix 'file://', it should be automatically removed
-		##############################################################################################
-		#returns empty line if no file:// prefix
-		#VOC1_INPUT_TEXT_PATH="$(echo "VOC1_INPUT_TEXT_PATH_ORIG" | awk -F 'file://' '{print $2}')"
-		##############################################################################################
-fi
-#input_text="$VOC1_INPUT_TEXT_PATH"
-input_text="$VOC1_INPUT_TEXT_PATH_ORIG"
+#if [[ -z "$VOC1_INPUT_TEXT_PATH" ]]
+	#then
+		#echo "input the path to the input text file and press enter"
+		#read VOC1_INPUT_TEXT_PATH_ORIG
+		## when copying a file in the dolphin file manager and then pasting to the text field, the output has a prefix 'file://', it should be automatically removed
+		###############################################################################################
+		##returns empty line if no file:// prefix
+		##VOC1_INPUT_TEXT_PATH="$(echo "VOC1_INPUT_TEXT_PATH_ORIG" | awk -F 'file://' '{print $2}')"
+		###############################################################################################
+#fi
+##input_text="$VOC1_INPUT_TEXT_PATH"
+#input_text="$VOC1_INPUT_TEXT_PATH_ORIG"
 
-cat "$input_text" | sed "s/[^a-zA-Z']/ /g" | tr ' ' '\n' | sed '/^\s*$/d' | sort | uniq >voc1-0words.list
+#cat "$input_text" | sed "s/[^a-zA-Z']/ /g" | tr ' ' '\n' | sed '/^\s*$/d' | sort | uniq >voc1-0words.list
 
 type="$1"
 word="$2"
+grep_exp="$3"
+
 if [[ $type = "ant" ]]
 	then
 		type_grep="Ant"
@@ -40,36 +42,40 @@ if [[ $type = "ant" ]]
 				type_grep="Syn"
 		fi
 fi
-while read line
+echo "======================="
+echo WORD/LINE: $line
+echo "======================="
+
+rm -fv voc1-1.list voc1-2.list
+#voc1-4.list
+# first, list all word's synonyms or antonyms to a file line-by-line
+## sdcv -n "launches" | grep "Syn:" -A1 | grep -v 'Syn:' | grep -v '\-\-'
+sdcv -n "$word" | grep "${type_grep}:" -A1 | grep -v "${type_grep}:" | grep -v '\-\-' | tr ',' ' ' | tr ';' ' ' | tr '/' ' ' | tr ',' ' ' | tr ' ' "\n" | sed '/^\s*$/d' | sort | uniq | sed 's~[^[:alnum:]/]\+~~g' >voc1-1.list
+## tail -n 1 | tr ',' ' ' | tr ';' ' ' | tr '/' ' ' | tr ',' ' ' | tr ' ' "\n" | sed '/^\s*$/d' | sort | uniq >voc1-1.list
+
+# now find synonyms for every previously found word to enlarge the word base
+while read -r line
 do
-	rm -fv voc1-1.list voc1-2.list voc1-3.list voc1-4.list
-	# first, list all word's synonyms or antonyms to a file line-by-line
-	## sdcv -n "launches" | grep "Syn:" -A1 | grep -v 'Syn:' | grep -v '\-\-'
-	sdcv -n "$word" | grep "${type_grep}:" -A1 | grep -v "${type_grep}:" | grep -v '\-\-' | tr ',' ' ' | tr ';' ' ' | tr '/' ' ' | tr ',' ' ' | tr ' ' "\n" | sed '/^\s*$/d' | sort | uniq >voc1-1.list
-	## tail -n 1 | tr ',' ' ' | tr ';' ' ' | tr '/' ' ' | tr ',' ' ' | tr ' ' "\n" | sed '/^\s*$/d' | sort | uniq >voc1-1.list
+	sdcv -n "$line" | grep "Syn:" -A1 | grep -v "Syn:" | grep -v '\-\-' | tr ',' ' ' | tr ';' ' ' | tr '/' ' ' | tr ',' ' ' | tr ' ' "\n" | sed '/^\s*$/d' | sort | uniq | sed 's~[^[:alnum:]/]\+~~g' >>voc1-2.list
+	## sdcv -n "$line" | grep 'Syn:' -A1 | tail -n 1 | tr ',' ' ' | tr ';' ' ' | tr '/' ' ' | tr ',' ' ' | tr ' ' "\n" | sed '/^\s*$/d' | sort | uniq >>voc1-2.list
+done < voc1-1.list
 
-	# now find synonyms for every previously found word to enlarge the word base
-	while read -r line
-	do
-		sdcv -n "$line" | grep "Syn:" -A1 | grep -v "Syn:" | grep -v '\-\-' | tr ',' ' ' | tr ';' ' ' | tr '/' ' ' | tr ',' ' ' | tr ' ' "\n" | sed '/^\s*$/d' | sort | uniq >>voc1-2.list
-		## sdcv -n "$line" | grep 'Syn:' -A1 | tail -n 1 | tr ',' ' ' | tr ';' ' ' | tr '/' ' ' | tr ',' ' ' | tr ' ' "\n" | sed '/^\s*$/d' | sort | uniq >>voc1-2.list
-	done < voc1-1.list
+# now find synonyms for each of already found words from 2 previous iterations using WordNet to enlarge our word base even more
+while read -r line
+do
+	wn "$line" -synsv -synsn -synsa -synsa | grep '=>' | awk -F '=> ' '{print $2}' | tr ', ' '\n' | sed '/^\s*$/d' | sort | uniq | sed 's~[^[:alnum:]/]\+~~g' >>voc1-3.list
+done < voc1-2.list
 
-	# now find synonyms for each of already found words from 2 previous iterations using WordNet to enlarge our word base even more
-	while read -r line
-	do
-		wn "$line" -synsv -synsn -synsa -synsa | grep '=>' | awk -F '=> ' '{print $2}' | tr ', ' '\n' | sed '/^\s*$/d' | sort | uniq >>voc1-3.list
-	done < voc1-2.list
+## oh, and now we will look for for each word's synonym in our first dictionary
+#while read -r line
+#do
+	#sdcv -n "$line" | grep "Syn:" -A1 | grep -v "Syn:" | grep -v '\-\-' | tr ',' ' ' | tr ';' ' ' | tr '/' ' ' | tr ',' ' ' | tr ' ' "\n" | sed '/^\s*$/d' | sort | uniq >>voc1-4.list
+	### sdcv -n "$line" | grep 'Syn:' -A1 | tail -n 1 | tr ',' ' ' | tr ';' ' ' | tr '/' ' ' | tr ',' ' ' | tr ' ' "\n" | sed '/^\s*$/d' | sort | uniq >>voc1-4.list
+#done < voc1-3.list
 
-	# oh, and now we will look for for each word's synonym in our first dictionary
-	while read -r line
-	do
-		sdcv -n "$line" | grep "Syn:" -A1 | grep -v "Syn:" | grep -v '\-\-' | tr ',' ' ' | tr ';' ' ' | tr '/' ' ' | tr ',' ' ' | tr ' ' "\n" | sed '/^\s*$/d' | sort | uniq >>voc1-4.list
-		## sdcv -n "$line" | grep 'Syn:' -A1 | tail -n 1 | tr ',' ' ' | tr ';' ' ' | tr '/' ' ' | tr ',' ' ' | tr ' ' "\n" | sed '/^\s*$/d' | sort | uniq >>voc1-4.list
-	done < voc1-3.list
-done < voc1-0words.list
 
-cat voc1-4.list >>voc1-5total.list
+cat voc1-3.list | sort | uniq | sed 's~[^[:alnum:]/]\+~~g' >>voc1-5total.list
+cat voc1-5total.list | grep ^${grep_exp} | sort -r
 
 # now we have many words one-by-line in the file voc1-3.list (in /tmp/voc1-3.list, if the directory /tmp exists)
 # now we will grep them using our mask to list the ones that match the crossword clue
